05d387dcde09923a30917c3f06ba0326a2b19a3c
sebastian <sebastiandev.img@gmail.com>
2023-12-07 22:51:59 -0300

    T8S2-158:endpoint para crear un deposito

--- src/main/java/com/alkemy/wallet/service/TransactionServiceImpl.java
+++ src/main/java/com/alkemy/wallet/service/TransactionServiceImpl.java
@@ -1,13 +1,18 @@
 package com.alkemy.wallet.service;
 
 import com.alkemy.wallet.dto.TransactionDto;
+import com.alkemy.wallet.dto.request.DepositRequestDto;
+import com.alkemy.wallet.dto.response.TransactionResponseDto;
 import com.alkemy.wallet.entity.Account;
 import com.alkemy.wallet.entity.Transaction;
 import com.alkemy.wallet.entity.User;
+import com.alkemy.wallet.enums.ETransactionType;
+import com.alkemy.wallet.repository.IAccountRepository;
 import com.alkemy.wallet.repository.ITransactionRepository;
 import com.alkemy.wallet.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +23,16 @@ public class TransactionServiceImpl implements ITransactionService {
 
     @Autowired
     private IUserRepository userRepository;
+    private final IJwtService jwtService;
+    private final ITransactionRepository transactionRepository;
+    private final IAccountRepository accountRepository;
+
+    public TransactionServiceImpl(JwtServiceImpl jwtService, ITransactionRepository transactionRepository, IAccountRepository accountRepository) {
+        this.jwtService = jwtService;
+        this.transactionRepository = transactionRepository;
+        this.accountRepository = accountRepository;
+    }
+
     @Override
     public List<TransactionDto> getTransactionsByUserId(Long id){
         Optional<User> optionalUser=userRepository.findById(id);
@@ -41,6 +56,46 @@ public class TransactionServiceImpl implements ITransactionService {
                 }
             }
             return transactionsDto;
+        }
+        return null;
+    }
+
+    @Override
+    public TransactionResponseDto createDeposit(DepositRequestDto depositRequest, String token) {
+        if(depositRequest.getAmount() < 0.00){
+            return null;
+        }
+        String userEmail = jwtService.extractUsername(token.substring(7));
+        Optional<User> userOptional = userRepository.findByEmail(userEmail);
+        if(userOptional.isPresent()){
+            User user = userOptional.get();
+            List<Account> userAccounts = user.getAccounts();
+            Optional<Account> accountOptional = userAccounts.stream()
+                    .filter(account -> account.getCurrency().name().equals(depositRequest.getCurrency()))
+                    .findFirst();
+            if(accountOptional.isPresent()){
+                Account account = accountOptional.get();
+                Transaction newTransaction = new Transaction();
+                newTransaction.setAmount(depositRequest.getAmount());
+                newTransaction.setType(ETransactionType.DEPOSIT);
+                newTransaction.setDescription(StringUtils.hasText(depositRequest.getDescription()) ? depositRequest.getDescription() : "");
+                newTransaction.setAccount(account);
+                Transaction transactionCreated = transactionRepository.save(newTransaction);
+
+                account.setBalance(account.getBalance() + depositRequest.getAmount());
+                accountRepository.save(account);
+                return new TransactionResponseDto(
+                        user.getEmail(),
+                        account.getId(),
+                        transactionCreated.getId(),
+                        depositRequest.getCurrency(),
+                        ETransactionType.DEPOSIT.name(),
+                        transactionCreated.getAmount(),
+                        transactionCreated.getDescription(),
+                        transactionCreated.getTransactionDate()
+                );
+
+            }
         }
         return null;
     }
