5ea2eabfe9a1720495f43d4f5f8cca14fe34bd82
sebastian <sebastiandev.img@gmail.com>
2023-12-08 22:14:39 -0300

    T8S2-162:endpoint para obtener una transaccion

--- src/main/java/com/alkemy/wallet/service/ITransactionService.java
+++ src/main/java/com/alkemy/wallet/service/ITransactionService.java
@@ -1,9 +1,11 @@
 package com.alkemy.wallet.service;
 
 import com.alkemy.wallet.dto.TransactionDto;
+import com.alkemy.wallet.dto.response.TransactionResponseDto;
 
 import java.util.List;
 
 public interface ITransactionService {
+    TransactionResponseDto getTransaction(Long id, String token);
     List<TransactionDto> getTransactionsByUserId(Long id);
 }
--- src/main/java/com/alkemy/wallet/service/TransactionServiceImpl.java
+++ src/main/java/com/alkemy/wallet/service/TransactionServiceImpl.java
@@ -1,9 +1,11 @@
 package com.alkemy.wallet.service;
 
 import com.alkemy.wallet.dto.TransactionDto;
+import com.alkemy.wallet.dto.response.TransactionResponseDto;
 import com.alkemy.wallet.entity.Account;
 import com.alkemy.wallet.entity.Transaction;
 import com.alkemy.wallet.entity.User;
+import com.alkemy.wallet.enums.ERole;
 import com.alkemy.wallet.repository.ITransactionRepository;
 import com.alkemy.wallet.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +13,50 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class TransactionServiceImpl implements ITransactionService {
 
-    @Autowired
-    private IUserRepository userRepository;
+
+    private final IUserRepository userRepository;
+    private final ITransactionRepository transactionRepository;
+    private final IJwtService jwtService;
+
+    public TransactionServiceImpl(IUserRepository userRepository,ITransactionRepository transactionRepository, JwtServiceImpl jwtService) {
+        this.userRepository = userRepository;
+        this.transactionRepository = transactionRepository;
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public TransactionResponseDto getTransaction(Long id, String token) {
+        Optional<Transaction> transactionOptional = transactionRepository.findById(id);
+        if (transactionOptional.isPresent()) {
+            Transaction transaction = transactionOptional.get();
+            User transactionOwner = transaction.getAccount().getUser();
+            String userEmail = jwtService.extractUsername(token.substring(7));
+            Optional<User> userOptional = userRepository.findByEmail(userEmail);
+            if (userOptional.isPresent()) {
+                User user = userOptional.get();
+                if (user.getRole().getName() == ERole.ADMIN || Objects.equals(transactionOwner.getId(), user.getId())) {
+                    return new TransactionResponseDto(
+                            transactionOwner.getEmail(),
+                            transaction.getAccount().getId(),
+                            transaction.getId(),
+                            transaction.getAccount().getCurrency().name(),
+                            transaction.getType().name(),
+                            transaction.getAmount(),
+                            transaction.getDescription(),
+                            transaction.getTransactionDate()
+                    );
+                }
+            }
+        }
+        return null;
+    }
+
     @Override
     public List<TransactionDto> getTransactionsByUserId(Long id){
         Optional<User> optionalUser=userRepository.findById(id);
