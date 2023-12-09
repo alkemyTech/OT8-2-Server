6b038ab210dadc6bd5ac0f074e68cbb61f0ecc6b
sebastian <sebastiandev.img@gmail.com>
2023-12-09 00:05:46 -0300

    T8S2-170:paginar listado de cuentas

--- src/main/java/com/alkemy/wallet/service/AccountServiceImpl.java
+++ src/main/java/com/alkemy/wallet/service/AccountServiceImpl.java
@@ -5,7 +5,11 @@ import java.util.List;
 import java.util.Optional;
 
 import com.alkemy.wallet.dto.BalanceDto;
+import com.alkemy.wallet.dto.response.PageableAccountResponseDto;
+import com.alkemy.wallet.repository.IAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import com.alkemy.wallet.dto.AccountDto;
 import com.alkemy.wallet.entity.Account;
@@ -15,8 +19,45 @@ import com.alkemy.wallet.repository.IUserRepository;
 @Service
 public class AccountServiceImpl implements IAccountService {
 
-    @Autowired
-    private IUserRepository userRepository;
+
+    private final IUserRepository userRepository;
+    private final IAccountRepository accountRepository;
+
+    public AccountServiceImpl(IUserRepository userRepository, IAccountRepository accountRepository) {
+        this.userRepository = userRepository;
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public PageableAccountResponseDto getAllAccounts(int page) {
+        int pageToFind = page > 0 ? page-1 : 0;
+        PageRequest pr = PageRequest.of(pageToFind,10);
+        Page<Account> accountPage = accountRepository.findAll(pr);
+        long count = accountPage.getTotalElements();
+        int pages = accountPage.getTotalPages();
+        String prevPage = accountPage.hasPrevious() ? "/api/v1/users?page="+(page-1) : null;
+        String nextPage = accountPage.hasNext() ? "/api/v1/users?page="+(page+1) : null;
+        if(pages < page){
+            return null;
+        }
+        List<Account> accounts = accountPage.getContent();
+        List<AccountDto> accountsDto = accounts.stream().map(account -> {
+            return new AccountDto(
+                    account.getUser().getEmail(),
+                    account.getId(),
+                    account.getCurrency().name(),
+                    account.getBalance(),
+                    account.getTransactionLimit()
+            );
+        }).toList();
+        return new PageableAccountResponseDto(
+                count,
+                pages,
+                prevPage,
+                nextPage,
+                accountsDto
+        );
+    }
 
     @Override
     public List<AccountDto> getAccountsByUserId(Long id) {
--- src/main/java/com/alkemy/wallet/service/IAccountService.java
+++ src/main/java/com/alkemy/wallet/service/IAccountService.java
@@ -3,9 +3,10 @@ package com.alkemy.wallet.service;
 import java.util.List;
 import com.alkemy.wallet.dto.AccountDto;
 import com.alkemy.wallet.dto.BalanceDto;
+import com.alkemy.wallet.dto.response.PageableAccountResponseDto;
 
 public interface IAccountService {
-    
+    PageableAccountResponseDto getAllAccounts(int page);
     List<AccountDto> getAccountsByUserId(Long id);
     List<BalanceDto> getBalanceById(Long id);
 
