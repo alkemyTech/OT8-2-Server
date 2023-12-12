02ad34e04f5948ffac138bbf58984dd649a2e52e
Sebastian Perez <103871085+sebadev-img@users.noreply.github.com>
2023-12-12 18:52:54 -0300

    T8S2-171:Paginar listado de transacciones por usuario (#43)
    * fixes:se arreglaron errores de importacion
    
    * T8S2-159:endpoint para hacer un pago
    
    * T8S2-171:paginar listado de transacciones

--- src/main/java/com/alkemy/wallet/service/ITransactionService.java
+++ src/main/java/com/alkemy/wallet/service/ITransactionService.java
@@ -3,17 +3,16 @@ package com.alkemy.wallet.service;
 import com.alkemy.wallet.dto.TransactionDto;
 import com.alkemy.wallet.dto.request.UpdateTransactionRequestDto;
 import com.alkemy.wallet.dto.request.TransactionRequestDto;
+import com.alkemy.wallet.dto.response.PageableTransactionResponseDto;
+import com.alkemy.wallet.dto.response.TransactionResponseDto;
 import com.alkemy.wallet.dto.request.DepositRequestDto;
 import com.alkemy.wallet.dto.response.TransactionResponseDto;
 import java.util.List;
 
 public interface ITransactionService {
-
-
-
     TransactionDto updateTransactionDescription(Long id, UpdateTransactionRequestDto updateRequest, String token);
     TransactionResponseDto getTransaction(Long id, String token);
-    List<TransactionDto> getTransactionsByUserId(Long userId);
+    PageableTransactionResponseDto getTransactionsByUserId(Long userId, int page,String token);
     TransactionResponseDto createDeposit(TransactionRequestDto depositRequest, String token);
     TransactionResponseDto createPayment(TransactionRequestDto paymentRequest, String token);
 
