9bc59834d99a850a02957dea7c81e37164e6ad1a
sebastian <sebastiandev.img@gmail.com>
2023-12-11 22:56:44 -0300

    T8S2-159:endpoint para hacer un pago

--- src/main/java/com/alkemy/wallet/service/ITransactionService.java
+++ src/main/java/com/alkemy/wallet/service/ITransactionService.java
@@ -2,7 +2,7 @@ package com.alkemy.wallet.service;
 
 import com.alkemy.wallet.dto.TransactionDto;
 import com.alkemy.wallet.dto.request.UpdateTransactionRequestDto;
-import com.alkemy.wallet.dto.request.DepositRequestDto;
+import com.alkemy.wallet.dto.request.TransactionRequestDto;
 import com.alkemy.wallet.dto.response.TransactionResponseDto;
 import java.util.List;
 
@@ -11,9 +11,9 @@ public interface ITransactionService {
 
 
     TransactionDto updateTransactionDescription(Long id, UpdateTransactionRequestDto updateRequest, String token);
-
     TransactionResponseDto getTransaction(Long id, String token);
     List<TransactionDto> getTransactionsByUserId(Long userId);
-    TransactionResponseDto createDeposit(DepositRequestDto depositRequest, String token);
+    TransactionResponseDto createDeposit(TransactionRequestDto depositRequest, String token);
+    TransactionResponseDto createPayment(TransactionRequestDto paymentRequest, String token);
 
 }
