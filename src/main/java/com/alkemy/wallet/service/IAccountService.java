b7500d1d93a53575ffd9c3811d4f6b2710e07852
sebastian <sebastiandev.img@gmail.com>
2023-12-12 00:15:05 -0300

    T8S2-150:se agrego la logica de autenticacion

--- src/main/java/com/alkemy/wallet/service/IAccountService.java
+++ src/main/java/com/alkemy/wallet/service/IAccountService.java
@@ -5,12 +5,11 @@ import com.alkemy.wallet.dto.AccountDto;
 import com.alkemy.wallet.dto.BalanceDto;
 import com.alkemy.wallet.dto.request.UpdateAccountRequestDto;
 import com.alkemy.wallet.dto.response.PageableAccountResponseDto;
-import com.alkemy.wallet.enums.ECurrency;
 
 
 public interface IAccountService {
     PageableAccountResponseDto getAllAccounts(int page);
-    List<AccountDto> getAccountsByUserId(Long id);
+    List<AccountDto> getAccountsByUserId(Long id,String token);
     List<BalanceDto> getBalanceById(Long id);
     AccountDto updateTransactionLimit(Long id, UpdateAccountRequestDto updateRequest, String token);
     AccountDto createAccount(String currency, String token);
